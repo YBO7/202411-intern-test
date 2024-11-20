3326fd80da9d259a128214ac26a5d538a03b5d1c
Yubin Oh <sale506559@gmail.com>
2024-11-20 16:44:06 +0900

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
 const app = express();
 const port = 4000;
 
+const client = new pg.Pool({
+    user: "postgres",
+    host: "localhost", 
+    password: "a123",
+    port: 5432,
+    database: "World",
+    // ssl: { rejectUnauthorized: false },
+});
+
 app.use(cors());
 
-// 기본 라우트 테스트
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Ready!');
+});
+
+
+app.get('/apple', async (req, res) => {
+    await client.connect();
+    const result = await client.query(
+        'SELECT id, img FROM public.fruit_world'
+    );
+    // console.log(result.rows[1].img);
+    res.send(result.rows[0].img)
 });
 
 app.listen(port, () => {
