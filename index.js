6db8c90c7e790f0cfe6bf84342dad9590700d9f1
Yubin Oh <sale506559@gmail.com>
2024-11-20 16:50:37 +0900

    feat1:1120

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const pg = require('pg');
 const app = express();
-const port = 4000;
+const port = 4001;
 
 const client = new pg.Pool({
-    user: "postgres",
-    host: "localhost", 
-    password: "a123",
+    // user: "postgres",
+    // host: "localhost", 
+    // password: "a123",
+    // port: 5432,
+    // database: "World",
+
+    user: "intern_db_8tlf_user",
+    host: "ldpg-csu2o6ggph6c73850ov0-a.oregon-postgres.render.com", 
+    password: "MsFNZUhSb1LWUFl2zuMROSumqKs3vJxc",
     port: 5432,
-    database: "World",
+    database: "intern_db_8tlf",
+    ssl: true,
     // ssl: { rejectUnauthorized: false },
 });
 
@@ -20,13 +27,13 @@ app.get('/', (req, res) => {
 });
 
 
-app.get('/apple', async (req, res) => {
+app.get('/menu', async (req, res) => {
     await client.connect();
     const result = await client.query(
-        'SELECT id, img FROM public.fruit_world'
+        'SELECT * FROM public.fruit_world'
     );
     // console.log(result.rows[1].img);
-    res.send(result.rows[0].img)
+    res.send(result.rows);
 });
 
 app.listen(port, () => {
